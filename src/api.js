539f253c05dc28c7d4299031dd5a1bbd82f09347
mark.tiahrt@concentrix.com <mark.tiahrt@concentrix.com>
2020-12-17 11:27:29 -0700

    added first action

--- src/api.js
+++ src/api.js
@@ -1,24 +1,27 @@
-//base URL
-const base_url = 'https://api.rawg.io/api/';
+  
+//Base URL
+const base_url = "https://api.rawg.io/api/";
 
-//get the date
+//Getting the date
 const getCurrentMonth = () => {
-    const month = new Date().getMonth() + 1;
-    if(month < 10){
-        return `0${month}`;
-    }
+  const month = new Date().getMonth() + 1;
+  if (month < 10) {
+    return `0${month}`;
+  } else {
     return month;
-}
-
+  }
+};
+//Getting the date
 const getCurrentDay = () => {
-    const day = new Date().getDate();
-    if(day < 10){
-        return `0${day}`;
-    }
+  const day = new Date().getDate();
+  if (day < 10) {
+    return `0${day}`;
+  } else {
     return day;
-}
+  }
+};
 
-//current day/month/year
+//Current day/month/year
 const currentYear = new Date().getFullYear();
 const currentMonth = getCurrentMonth();
 const currentDay = getCurrentDay();
@@ -26,7 +29,7 @@ const currentDate = `${currentYear}-${currentMonth}-${currentDay}`;
 const lastYear = `${currentYear - 1}-${currentMonth}-${currentDay}`;
 const nextYear = `${currentYear + 1}-${currentMonth}-${currentDay}`;
 
-//populate games
-const popular_games = `games?dates=${lastYear},${currentDate}&ordering=-rating&page_size=20`;
+//Popular Games
+const popular_games = `games?dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`;
 
-export const popularGamesURL = () => `${base_url}${popular_games}`
+export const popularGamesURL = () => `${base_url}${popular_games}`;
